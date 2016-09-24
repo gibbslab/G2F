54784ca4e9f15e80d208bbe796f719ad59d2772c
Daniel Osorio <dcosorioh@unal.edu.co>
2016-09-24 10:53:13 -0500

    Update g2f
    New functions from minval were added

--- R/gapFill.R
+++ R/gapFill.R
@@ -8,14 +8,14 @@ gapFill <- function(reactionList, reference, limit = 0.5, woCompartment=FALSE,co
     reactionList <- gsub("\\[[[:alpha:]]\\]","",reactionList)
   }
   reactions <- reactionList
-  orphan <- orphan.reactants(reactions)
+  orphan <- orphanReactants(reactions)
   repeat{
-    orphan_r <- orphan.reactants(reactions)
+    orphan_r <- orphanReactants(reactions)
     orphan_r <- orphan_r[orphan_r%in%orphan]
     message(paste0(length(orphan_r)," Orphan reactants found"))
     to.add <- unique(unlist(sapply(orphan_r, function(metabolite){.reactant.fill(metabolite,reference)})))
     rxn <- unique(c(reactions,to.add[unlist(sapply(to.add, function(reaction){.addition.cost(reaction,reactions)}))<limit]))
-    if(length(orphan.reactants(rxn))<length(orphan_r)){
+    if(length(orphanReactants(rxn))<length(orphan_r)){
       reactions <- unique(c(reactions,rxn))
     } else {
       break
@@ -23,14 +23,14 @@ gapFill <- function(reactionList, reference, limit = 0.5, woCompartment=FALSE,co
   }
   oreactants <- reactions
   reactions <- reactionList
-  orphan <- orphan.products(reactions)
+  orphan <- orphanProducts(reactions)
   repeat{
-    orphan_p <- orphan.products(reactions)
+    orphan_p <- orphanProducts(reactions)
     orphan_p <- orphan_p[orphan_p%in%orphan]
     message(paste0(length(orphan_p)," Orphan products found"))
     to.add <- unique(unlist(sapply(orphan_p, function(metabolite){.product.fill(metabolite,reference)})))
     rxn <- unique(c(reactions,to.add[unlist(sapply(to.add, function(reaction){.addition.cost(reaction,reactions)}))<limit]))
-    if(length(orphan.products(rxn))<length(orphan_p)){
+    if(length(orphanProducts(rxn))<length(orphan_p)){
       reactions <- unique(c(reactions,rxn))
     } else {
       break
