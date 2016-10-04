2b6cdc6eb31cc0356f2e5ead0b9ca447f141b2ab
Daniel Osorio <dcosorioh@unal.edu.co>
2016-10-04 18:39:12 -0500

    Update g2f

--- R/blockedReactions.R
+++ R/blockedReactions.R
@@ -1,8 +1,18 @@
 #' @export blockedReactions
 #' @author Andrés Pinzón <ampinzonv@unal.edu.co> - Mantainer: Daniel Camilo Osorio Hurtado <dcosorioh@unal.edu.co>
+#' @title Identify blocked reactions in a metabolic network
 #  Bioinformatics and Systems Biology Lab      | Universidad Nacional de Colombia
 #  Experimental and Computational Biochemistry | Pontificia Universidad Javeriana
-
+#' @description A blocked reaction in a metabolic network is a reaction that not participate in any optimization solution. This function set as objective function each one of the reactions (one by time) in the model, and identifies the reactions without flux under all scenarios.
+#' @param model A valid model for the \code{'sybil'} package. An object of class modelorg.
+#' @return A vector with the reaction ids of the blocked reactions
+#' @examples
+#' # Loading a model for the 'sybil' package
+#' data("Ec_core")
+#' 
+#' # Identifying blocked reactions
+#' blockedReactions(Ec_core)
+#' @keywords Blocked reactions genome scale metabolic reconstruction
 blockedReactions <- function(model){
   if(!is.loaded("sybil")){require("sybil")}
   locked <- NULL
