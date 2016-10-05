3f6ba82ffc69e39631d03db171bf7d5d10ad927e
Daniel Osorio <dcosorioh@unal.edu.co>
2016-10-05 11:36:08 -0500

    Update g2f

--- R/additionCost.R
+++ R/additionCost.R
@@ -1,6 +1,8 @@
 #' @export additionCost
 #' @author Daniel Camilo Osorio <dcosorioh@unal.edu.co>
-#' @title Calculate the addition cost of a stoichiometric reaction in a metabolic network
+#' @title Calculate the cost of addition of a stoichiometric reaction in a metabolic network
+#' @description For a given set of stoichiometric reactions this function calculates the cost of addition in a reference metabolic network. 
+#' The cost is calculated by dividing the amount of non included metabolites in the reference metabolic network over the total number of metabolites involved in the reaction.
 additionCost <- function(reaction,reference){
   mets_r <- metabolites(reference)
   mets <- lapply(reaction,metabolites)
