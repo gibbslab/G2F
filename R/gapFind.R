a866b50b0f1ae7794c725b87222ccee9f1be54c0
Daniel Osorio <dcosorioh@tamu.edu>
2018-06-08 16:08:37 -0500

    Update gapFind

--- R/gapFind.R
+++ R/gapFind.R
@@ -1,5 +1,5 @@
 #' @export gapFind
-#' @importFrom "minval" "orphanMetabolites"
+#' @importFrom "minval" "orphanMetabolites" "rearmReactions"
 #' @author Daniel Osorio <dcosorioh@unal.edu.co>
 #' @title Find gaps in a metabolic network
 #' @description This function identifies the gaps (metabolites not produced or not consumed in any other reaction or just involved in one reaction) for a set of stoichometric reactions
