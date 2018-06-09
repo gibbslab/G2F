e313b202f908ebe4325c30362f2cc7fd405899e3
Daniel Osorio <dcosorioh@tamu.edu>
2018-06-08 19:12:23 -0500

    gapFind updated

--- R/gapFind.R
+++ R/gapFind.R
@@ -1,13 +1,11 @@
 #' @export gapFind
-#' @importFrom "minval" "orphanMetabolites"
+#' @importFrom "minval" "orphanReactants" "orphanProducts"
 #' @author Daniel Osorio <dcosorioh@unal.edu.co>
 #' @title Find gaps in a metabolic network
 #' @description This function identifies the gaps (metabolites not produced or not consumed in any other reaction or just involved in one reaction) for a set of stoichometric reactions
 gapFind <- function(reactionList, removeExchange = FALSE){
   if(class(reactionList) == "modelorg"){
-    stoichiometricMatrix <- reactionList@S
-    rownames(stoichiometricMatrix) <- reactionList@met_id
-    reactionList <- minval:::rearmReactions(S = stoichiometricMatrix, reversible = reactionList@react_rev)
+    reactionList <- model2string(model = reactionList)
   }
   if (class(reactionList) == "character"){
     if(removeExchange == TRUE){
